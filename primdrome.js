81f723da303949ec8bfd8c794fd30be302bc5568
Prabhat kumar gupta <guptajim3636@gmail.com>
2022-10-23 12:04:56 +0530

    palim problem

---
+++ primdrome.js
@@ -0,0 +1,15 @@
+let str="ram";
+let jhola="";
+for(i=0;i<=str.length-1;i++)
+  {
+  jhola += str[i];
+  }
+if(jhola==str)
+   {
+     console.log("palindrome") ;
+   }
+ else
+  {
+    console.log("not palim");
+  }
+  
